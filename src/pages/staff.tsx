3740e45ecae2a1d1af5371eff69235ceb38c1419
mkdirk <112687575+mkdirk@users.noreply.github.com>
2024-07-30 23:40:37 +0700

    Update staff.tsx

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -1,48 +1,75 @@
 import Layout from "../components/layout";
-import cafeBackgroundImage from "../assets/images/bg-cafe-1.jpg";
+import cafeBackgroundImage from "../assets/images/bg-cafe-2.jpg";
+import useSWR from "swr";
+import { Order, Menu } from "../lib/models";
+import Loading from "../components/loading";
+import { Alert } from "@mantine/core";
+import { IconAlertTriangleFilled } from "@tabler/icons-react";
 
-import coffeeImage from "../assets/images/coffee-1.jpg";
 
-export default function HomePage() {
+export default function BooksPage() {
+  const { data: order, error } = useSWR<Order[]>("/staff");
+  const { data: menus} = useSWR<Menu[]>("/menu");
+
+  const menuMap = new Map<number, string>();
+  if (menus) {
+    menus.forEach(menu => menuMap.set(menu.id, menu.name));
+  }
+
   return (
-    <Layout>
-      <section
-        className="h-[500px] w-full text-white bg-orange-800 bg-cover bg-blend-multiply flex flex-col justify-center items-center px-4 text-center"
-        style={{
-          backgroundImage: `url(${cafeBackgroundImage})`,
-        }}
-      >
-        <h1 className="text-5xl mb-2">ยินดีต้อนรับสู่ IoT Library & Cafe</h1>
-        <h2>ร้านกาแฟที่มีหนังสืออยู่นิดหน่อยให้คุณได้อ่าน</h2>
-      </section>
+    <>
+      <Layout>
+        <section
+          className="h-[500px] w-full text-white bg-orange-800 bg-cover bg-blend-multiply flex flex-col justify-center items-center px-4 text-center"
+          style={{
+            backgroundImage: `url(${cafeBackgroundImage})`,
+          }}
+        >
+          <h1 className="text-5xl mb-2">รายการเครื่องดื่มที่ลูกค้าสั่ง</h1>
+          <h2>รายการทั้งหมด</h2>
+        </section>
 
-      <section className="container mx-auto py-8">
-        <h1>เกี่ยวกับเรา</h1>
+        <section className="container mx-auto py-8">
+          <div className="flex justify-between">
+            <h1>รายการเครื่องดื่ม</h1>
 
-        <div className="grid grid-cols-3 gap-4">
-          <p className="text-left col-span-2">
-            IoT Library & Cafe เป็นร้านกาแฟที่มีหนังสืออยู่นิดหน่อยให้คุณได้อ่าน
-            และเรียนรู้เรื่องใหม่ๆ ที่เกี่ยวกับเทคโนโลยี IoT โดยคาเฟ่ของเรานั้น ก่อตั้งขึ้นโดย
-            ผศ.ดร. ปานวิทย์ ธุวะนุติ ซึ่งเป็นอาจารย์ในวิชา Internet of Things และนายกฤตณัฏฐ์
-            ศิริพรนพคุณ เป็นผู้ช่วยสอนในหัวข้อ FastAPI และ React ในวิชานี้
-          </p>
-
-          <div>
-            <img src="https://placehold.co/150x200" alt="Panwit Tuwanut" className="h-full w-full object-cover" />
+           
           </div>
-        </div>
-        <p className="text-right mt-8">
-          ปัจจุบันค่าเฟ่ และห้องสมุดของเรา อยู่ในช่วงการดูแลของ นายณัฐพงศ์ มาสำราญ 65070078
-          {/* TODO: ชื่อของตนเอง, รหัสประจำตัวนักศึกษา และแนะนำคาเฟ่นี้ต่ออีกสักหน่อย + ใส่รูปของตนเอง (ไม่จำเป็นหากไม่สะดวกใจใส่รูป) */}
-          ซึ่งมีบริการ ขายกาแฟสุดแสนจะวิเศษที่ร้านเราทำสดใหม่ทุกวัน 
-          และมีบริการหยิบยืมหนังสือให้คุณได้อ่านอย่างฟรีๆหาก
-          คุณตัดสินใจที่จะมาใช้บริการของร้านเรา
-        </p>
-      </section>
 
-      <section className="w-full flex justify-center">
-        <img src={coffeeImage} alt="Coffee" className="w-full" />
-      </section>
-    </Layout>
+          {!order && !error && <Loading />}
+          {error && (
+            <Alert
+              color="red"
+              title="เกิดข้อผิดพลาดในการอ่านข้อมูล"
+              icon={<IconAlertTriangleFilled />}
+            >
+              {error.message}
+            </Alert>
+          )}
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {order?.map((order) => (
+              <div className="border border-solid border-neutral-200" key={order.id}>
+                {/* <img
+                  src="https://placehold.co/150x200"
+                  alt={book.title}
+                  className="w-full object-cover aspect-[3/4]"
+                /> */}
+                <div className="p-4">
+                  <h2 className="text-lg font-semibold line-clamp-2">{menuMap.get(order.order_id)}</h2>
+                  <p className="text-xs text-neutral-500">ราคารวม {order.total_price}</p>
+                </div>
+
+                <div className="flex justify-end px-4 pb-2">
+                  {/* <Button component={Link} to={`/books/${book.id}`} size="xs" variant="default">
+                    ดูรายละเอียด
+                  </Button> */}
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </Layout>
+    </>
   );
 }
