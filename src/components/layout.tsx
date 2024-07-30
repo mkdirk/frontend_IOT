69d25eb8b96359cc86a7844c8c88e13029f8f474
mkdirk <112687575+mkdirk@users.noreply.github.com>
2024-07-30 19:05:13 +0700

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -31,12 +31,12 @@ export default function Layout({ children }: LayoutProps) {
                 หนังสือ
               </Link>
 
-              {/* <Link
+              <Link
                 to={"/menu"}
                 className="flex items-center h-14 px-1 no-underline text-neutral-600 font-semibold text-sm"
               >
                 เมนู
-              </Link> */}
+              </Link> 
             </Group>
 
             <div></div>
